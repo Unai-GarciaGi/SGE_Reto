9da4d5a16864593da20a0e3083d09af962ae31fe
Unai García Giralde <unaibaraka@outlook.es>
2022-01-28 14:21:45 +0100

    MySQL

---
+++ XAMPPTest/XAMPPTest/Index.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Index.aspx.vb" Inherits="XAMPPTest.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            PAQUETES NUGGET<br />
+            MYSQL -&gt; DESCARGAR EL DE ORACLE (MAYOR DESCARGAS) Y EL CONNECTOR<br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ XAMPPTest/XAMPPTest/Index.aspx.vb
@@ -0,0 +1,48 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Index
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Creamos un string para conectarnos dandole el servidor, las credenciales y la base de datos
+        Dim connectionString As String = "Server=localhost;userid=root;password=;Database=northwind"
+        'Creamos la conexion utilizando la string anterior
+        Dim conexion As New MySqlConnection(connectionString)
+        Try
+            'Abrimos la conexion
+            conexion.Open()
+            Label1.Text = "Mysql server version: " & conexion.ServerVersion
+            Dim query As String = "SELECT company, first_name, last_name FROM customers"
+            'PARTE 1 -> LECTOR SOLO HACIA ADELANTE
+            'Creo una MySqlCommand con la conexion
+            'Dim cmd As MySqlCommand = conexion.CreateCommand()
+            'Asigno al comando la query
+            'cmd.CommandText = query
+            'Creo el reader con el comando, es de tipo MySqlDataReader
+            'Dim lector As MySqlDataReader = cmd.ExecuteReader
+            'Y comienzo a leer del lector solo hacia adelante
+            'While lector.Read()
+            'lector.getString(0) -> Devuelve el primer string (en este caso company)
+            'End While
+            'PARTE 2 -> DATA ADAPTER
+            'Creo un MySqlDataAdapter con la query y la conexion
+            Dim da As MySqlDataAdapter = New MySqlDataAdapter(query, conexion)
+            'Creo un dataset
+            Dim ds As DataSet = New DataSet()
+            'Lleno el dataset utilizando el adapter
+            da.Fill(ds, "customers")
+            'A partir de aquí funciona como cualquier otro dataset
+            'Puedo sacar la tabla completa
+            Dim tabla As DataTable = ds.Tables("customers")
+            'De esta tabla puedo luego ir sacando sus filas
+            Dim fila As DataRow = tabla.Rows(0)
+            'Y de esa fila el dato que quiera
+            Label2.Text = fila.ItemArray(0).ToString
+            Label3.Text = fila.ItemArray(1).ToString
+            Label4.Text = fila.ItemArray(2).ToString
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
